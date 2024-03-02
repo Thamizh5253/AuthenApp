2a6947c58b0dbd6713fa1a106ec50c838e0189fb
Thamizharasan P <thamizh5253@gmail.com>
2024-03-02 21:01:43 +0530

    Initial commit

---
+++ Authen/src/login.jsx
@@ -0,0 +1,274 @@
+import React, { useState } from "react";
+import "./login1.scss";
+
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+import axios from "axios";
+
+const login = () => {
+  const [credentials, setCredentials] = useState({ uid: "", password: "" });
+  const handleUidChange = (event) => {
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      uid: event.target.value,
+    }));
+  };
+
+  const handlePasswordChange = (event) => {
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      password: event.target.value,
+    }));
+  };
+  const [udata, setData] = useState({ ruid: "", rpassword: "" });
+  const [cpass, SetCpass] = useState("");
+  const handleUid = (event) => {
+    setData((prevCredentials) => ({
+      ...prevCredentials,
+      ruid: event.target.value,
+    }));
+  };
+  const handlePassSame = (event) => {
+    SetCpass(event.target.value);
+  };
+
+  const handlePass = () => {
+    toast.error("🪲 There is a BUG in backend", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+  const handlePassword = (event) => {
+    setData((prevCredentials) => ({
+      ...prevCredentials,
+      rpassword: event.target.value,
+    }));
+  };
+
+  const handleLogin = async () => {
+    // setCredentials(uid, password);
+    console.log(credentials);
+    // You can perform login logic here using uid and password
+    // console.log("Login clicked with UID:", uid, "and Password:", password);
+    try {
+      const response = await axios.post(
+        "http://localhost:3003/login",
+        credentials
+      );
+      console.log(response.status);
+      if (response.status === 200) {
+        toast.success("😍 Login successful", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          // transition: Bounce,
+        });
+      } else {
+        toast.warn("😒 Invalid Credentials", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          // transition: Bounce,
+        });
+      }
+    } catch (error) {
+      toast.error("🪲 There is a BUG in backend", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        // transition: Bounce,
+      });
+    }
+  };
+  const handleSignup = async () => {
+    // setCredentials(uid, password);
+    if (udata.rpassword === cpass) {
+      console.log(udata);
+      // You can perform login logic here using uid and password
+      // console.log("Login clicked with UID:", uid, "and Password:", password);
+      try {
+        const response = await axios.post(
+          "http://localhost:3003/signup",
+          udata
+        );
+        console.log("Data Added", response.data);
+        // You can handle the login success here
+        if (response.status === 201) {
+          toast.success("User created successfully", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            // transition: Bounce,
+          });
+        } else {
+          toast.warn("User Already Exists", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            // transition: Bounce,
+          });
+        }
+      } catch (error) {
+        toast.error("🪲 There is a BUG in backend", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          // transition: Bounce,
+        });
+      }
+    } else {
+      toast.warn("Password Same ah Illa BRO 😎", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        // transition: Bounce,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <section className="main">
+        <div className="form_wrapper">
+          <input
+            type="radio"
+            className="radio"
+            name="radio"
+            id="login"
+            defaultChecked="login"
+          />
+          <input type="radio" className="radio" name="radio" id="signup" />
+          <div className="tile">
+            <h3 className="login">Login Form</h3>
+            <h3 className="signup">Signup Form</h3>
+          </div>
+          <label className="tab login_tab" htmlFor="login">
+            Login
+          </label>
+          <label className="tab signup_tab" htmlFor="signup">
+            Signup
+          </label>
+          <span className="shape" />
+          <div className="form_wrap">
+            <div className="form_fild login_form">
+              <div className="input_group">
+                <input
+                  type="email"
+                  className="input"
+                  placeholder="Email Address"
+                  value={credentials.uid}
+                  onChange={handleUidChange}
+                />
+              </div>
+              <div className="input_group">
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  value={credentials.password}
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <a onClick={handlePass} className="forgot">
+                Forgot password?
+              </a>
+              <input
+                type="submit"
+                onClick={handleLogin}
+                className="btn"
+                defaultValue="Login"
+              />
+
+              <div className="not_mem">
+                <label htmlFor="signup">
+                  Not a member? <span> Signup now</span>
+                </label>
+              </div>
+            </div>
+            <div className="form_fild signup_form">
+              <div className="input_group">
+                <input
+                  type="email"
+                  className="input"
+                  placeholder="Email Address"
+                  value={udata.ruid}
+                  onChange={handleUid}
+                />
+              </div>
+              <div className="input_group">
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  value={udata.rpassword}
+                  onChange={handlePassword}
+                />
+              </div>
+              <div className="input_group">
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Confirm Password"
+                  value={cpass}
+                  onChange={handlePassSame}
+                />
+              </div>
+
+              <input
+                type="submit"
+                onClick={handleSignup}
+                className="btn"
+                defaultValue="Signup"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default login;
---
+++ backend/server.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3003;
+let likeCount = process.env.LIKE ? parseInt(process.env.LIKE, 10) : 0;
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// Enable CORS for all routes
+app.use(cors());
+// Simple array to store user credentials (for demonstration purposes)
+const users = [
+  { username: "user1", password: "password1" },
+  { username: "user2", password: "password2" },
+  { username: "tham", password: "123" },
+  { username: "Tom", password: "Jerry" },
+  { username: "Admin", password: "123" },
+];
+
+// Login route
+app.post("/login", (req, res) => {
+  const { uid, password } = req.body;
+  console.log(uid, password);
+  // Check if the provided credentials match any user
+  const user = users.find((u) => u.username === uid && u.password === password);
+
+  if (user) {
+    // Successful login
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    // Failed login
+    res.status(206).json({ message: "Invalid credentials" });
+  }
+});
+
+app.post("/signup", (req, res) => {
+  const { ruid, rpassword } = req.body;
+  console.log(ruid, rpassword);
+  // Check if the provided credentials match any user
+  //   const user = users.find((u) => u.username === uid && u.password === password);
+  const existingUser = users.find((u) => u.username === ruid);
+
+  if (existingUser) {
+    res.status(200).json({ message: "Username already exists" });
+  } else {
+    //   Add the new user to the array
+    users.push({ username: ruid, password: rpassword });
+    console.log(users);
+    res.status(201).json({ message: "User created successfully" });
+  }
+});
+
+// Simulating the like count
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Endpoint to get the like count
+app.get("/api/like-count", (req, res) => {
+  res.json({ likes: likeCount });
+  console.log(likeCount);
+});
+
+// Endpoint to increment the like count (for demonstration purposes)
+app.post("/api/increment-like", (req, res) => {
+  console.log(likeCount);
+  likeCount += 1;
+  res.status(200).json({ likes: likeCount });
+  console.log("after:", likeCount);
+});
+
+// const data = {
+//   message: "Hello from the server!",
+// };
+// app.get("/api/data", (req, res) => {
+//   res.json(data);
+// });
+
+// Start the server
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
